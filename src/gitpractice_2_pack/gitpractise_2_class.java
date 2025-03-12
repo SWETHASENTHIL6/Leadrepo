216cab0c814a9701eafa7b6f9ee6f47dd9c4d784
VigneshRamesh13997 <vigneshramesh1397@gmail.com>
2025-03-12 20:56:31 +0530

    basic code created...in the name of swe

---
+++ src/gitpractice_2_pack/gitpractise_2_class.java
@@ -0,0 +1,19 @@
+package gitpractice_2_pack;
+
+public class gitpractise_2_class {
+
+	public static void main(String[] args) {
+
+		System.out.println("swe is a gud grl");
+
+		System.out.println("she's intelligent");
+
+		System.out.println("she's genious");
+
+		System.out.println("above statements are false");
+
+	}
+
+}
+
+
